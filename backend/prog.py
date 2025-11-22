a1736e834c7930e89f4e181d4ee8c4b081278899
Berta Manau <bertamanau@gmail.com>
2025-11-22 22:05:10 +0100

    FUNCIONA

---
+++ backend/prog.py
@@ -0,0 +1,62 @@
+import csv
+
+# Obrim el CSV
+with open("DADES.csv", newline='', encoding="utf-8") as f:
+    reader = csv.reader(f)
+    files = list(reader)
+
+# Noms de les columnes
+header = files[0]
+requisits = header[1:9]   # EXACTAMENT 8 requisits
+
+# Dades dels barris
+barris = []
+valors = []
+
+for fila in files[1:]:
+    if not fila[0].strip():
+        continue
+
+    barris.append(fila[0])
+
+    fila_valors = []
+    for x in fila[1:9]:    # NOMÉS agafem les 8 columnes bones
+        try:
+            fila_valors.append(int(x))
+        except:
+            fila_valors.append(0)   # Si hi ha "ç" o buit → 0 automàtic
+    valors.append(fila_valors)
+
+print("\nRESPON AMB: 1 = Sí   |   -1 = No   |   0 = M'és igual\n")
+
+# Preguntes automàtiques
+respostes = []
+for req in requisits:
+    r = int(input(f"{req}? (1/-1/0): "))
+    respostes.append(r)
+
+# Comparació
+punts = []
+
+for i in range(len(barris)):
+    count = 0
+    fila_barri = valors[i]
+
+    for j in range(len(requisits)):
+        valor_usuari = respostes[j]
+        valor_barri = fila_barri[j]
+
+        if valor_usuari == 0:
+            continue
+        if valor_usuari == valor_barri:
+            count += 1
+
+    punts.append(count)
+
+# Trobar el guanyador
+max_index = punts.index(max(punts))
+millor_barri = barris[max_index]
+
+print("\n🏆  BARRI RECOMANAT:")
+print(millor_barri)
+print("Coincidències:", punts[max_index])
